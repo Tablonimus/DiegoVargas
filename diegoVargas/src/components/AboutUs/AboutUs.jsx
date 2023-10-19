436fd1c77d9debb15d0334ec163fefee299b73f8
tablonimus <lautamartin1995@gmail.com>
2023-10-19 18:23:01 -0300

    vargas reloaded

--- diegoVargas/src/components/AboutUs/AboutUs.jsx
+++ diegoVargas/src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,7 @@
-import { Carousel } from "flowbite-react";
 import React from "react";
-import dv1 from "../../assets/carousel/dv1.jpg";
-import dv2 from "../../assets/carousel/dv2.jpg";
-import dv3 from "../../assets/carousel/dv3.jpg";
-import dv4 from "../../assets/carousel/dv4.jpg";
-import dv5 from "../../assets/carousel/dv5.jpg";
+import dv2 from "../../assets/player/diegoMockUpL.png";
+import dv3 from "../../assets/player/diegoMockUpR.png";
+import dv4 from "../../assets/carousel/dv2.jpg";
 import logoSuela from "../../assets/logos/LogoSuelaNav.svg";
 
 export default function AboutUs() {
@@ -14,22 +11,25 @@ export default function AboutUs() {
         <span className="font-bold">SUELA PRODUCCIONES</span>
       </div>
       <div className="flex justify-center">
-        <div className=" relative flex flex-col h-56 md:h-96 w-2/3  md:w-10/12 bg-white/80 p-2 rounded-lg ">
+        <div className=" relative flex flex-col h-56 md:h-96 w-10/12  md:w-10/12 bg-white/80 p-2 rounded-lg ">
           <section className="flex flex-col gap-2 justify-between items-center h-full text-black py-2">
             <img
               src={logoSuela}
               alt=""
               className="w-20 md:w-36 drop-shadow-2xl"
             />
-            <span className="text-center text-sm md:text-xl">
+            <span className="text-center w-2/3 text-sm md:text-xl">
               Producciones digitales personalizadas
             </span>
-            <a href="https://www.instagram.com/suelaproducciones" target="_blank">
+            <a
+              href="https://www.instagram.com/suelaproducciones"
+              target="_blank"
+            >
               <button className="py-2 px-6 rounded-lg border hover:bg-cyan-400 bg-cyan-500 text-white drop-shadow-2xl">
                 Contacto
               </button>
             </a>
-            <span className="text-center italic text-sm md:text-3xl font-bold">
+            <span className="text-center italic text-sm md:text-3xl font-smibold">
               CONVERTITE EN PROFESIONAL
             </span>
           </section>
@@ -37,12 +37,12 @@ export default function AboutUs() {
           <img
             src={dv2}
             alt=""
-            className="absolute left-[-4em] bottom-12 md:h-48 md:w-32 h-30 w-20  object-cover rounded-lg"
+            className="absolute left-[-7em] md:left-[-20em] bottom-[0em] md:bottom-[-4em] md:h-[30em]  h-44  object-contain rounded-lg drop-shadow-2xl"
           />
           <img
             src={dv3}
             alt=""
-            className="absolute right-[-4em] bottom-12 md:h-48 md:w-32 h-30 w-20 object-cover rounded-lg"
+            className="absolute right-[-7em] md:right-[-18em] bottom-[1em] md:bottom-[-0.5em] md:h-[25em]  h-40 object-contain rounded-lg drop-shadow-2xl"
           />
         </div>
       </div>
